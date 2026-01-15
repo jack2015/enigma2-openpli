c0e58d1b771184c1923b09bdcbf44fdbdfc81326
jackgee2021 <344784320@qq.com>
2026-01-15 08:17:38 +0000

    update lib/python/Components/SystemInfo.py.
    Signed-off-by: jackgee2021 <344784320@qq.com>

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -47,7 +47,7 @@ SystemInfo["ISDREAMBOX"] = getBoxBrand() in ("dreambox",)
 SystemInfo["CanAC3plusTranscode"] = fileExists("/proc/stb/audio/ac3plus_choices")
 SystemInfo["CanWMAPRO"] = fileExists("/proc/stb/audio/wmapro")
 SystemInfo["CanDTSHD"] = fileExists("/proc/stb/audio/dtshd_choices")
-SystemInfo["DreamBoxAudio"] = model in ("dm7080", "dm800", 'dm900', 'dm920', 'dreamone', 'dreamtwo')
+SystemInfo["DreamBoxAudio"] = model in ("dm7080", "dm820", 'dm900', 'dm920', 'dreamone', 'dreamtwo')
 SystemInfo["InDebugMode"] = eGetEnigmaDebugLvl() >= 4
 SystemInfo["CommonInterface"] = model in ("h9combo", "h9combose", "h10","pulse4kmini") and 1 or eDVBCIInterfaces.getInstance().getNumOfSlots()
 SystemInfo["CommonInterfaceCIDelay"] = fileCheck("/proc/stb/tsmux/rmx_delay")
